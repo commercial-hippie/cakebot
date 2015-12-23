4884545794caf7fc2bf4ec0223262a1aa39424d1
Michael <michael@sascreative.com.au>
2015-12-23 11:39:22 +1100

    bake logstable

---
+++ src/Model/Table/LogsTable.php
@@ -0,0 +1,71 @@
+<?php
+namespace App\Model\Table;
+
+use App\Model\Entity\Log;
+use Cake\ORM\Query;
+use Cake\ORM\RulesChecker;
+use Cake\ORM\Table;
+use Cake\Validation\Validator;
+
+/**
+ * Logs Model
+ *
+ */
+class LogsTable extends Table
+{
+
+    /**
+     * Initialize method
+     *
+     * @param array $config The configuration for the Table.
+     * @return void
+     */
+    public function initialize(array $config)
+    {
+        parent::initialize($config);
+
+        $this->addBehavior('Timestamp');
+
+        $this->belongsTo('Channels');
+    }
+
+    /**
+     * Default validation rules.
+     *
+     * @param \Cake\Validation\Validator $validator Validator instance.
+     * @return \Cake\Validation\Validator
+     */
+    public function validationDefault(Validator $validator)
+    {
+        $validator
+            ->add('id', 'valid', ['rule' => 'numeric'])
+            ->allowEmpty('id', 'create');
+
+        $validator
+            ->requirePresence('channel', 'create')
+            ->notEmpty('channel');
+
+        $validator
+            ->requirePresence('username', 'create')
+            ->notEmpty('username');
+
+        $validator
+            ->requirePresence('text', 'create')
+            ->notEmpty('text');
+
+        return $validator;
+    }
+
+    /**
+     * Returns a rules checker object that will be used for validating
+     * application integrity.
+     *
+     * @param \Cake\ORM\RulesChecker $rules The rules object to be modified.
+     * @return \Cake\ORM\RulesChecker
+     */
+    public function buildRules(RulesChecker $rules)
+    {
+        $rules->add($rules->isUnique(['username']));
+        return $rules;
+    }
+}
